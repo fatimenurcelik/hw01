2e98fe47315adc47bcef16399f025ca9eac7749a
fatimenurcelik <fatimenurc@gmail.com>
2021-03-26 21:24:31 +0300

    changed

--- hw01.c
+++ hw01.c
@@ -10,37 +10,38 @@ int main(int argc, char*argv[])
     int **mul;
     int i,j,k,limit;
     limit=atoi(argv[1]);
-    
-    a=(int**)malloc(sizeof(int)*limit);
+    struct timeval start,stop;    
+
+    a=(int**)malloc(sizeof(int*)*limit);
     for(i=0;i<limit;i++){
-        a[i]=(int*)malloc(sizeof(int)*limit);
+        a[i]=(int*)malloc(sizeof(int*)*limit);
         for(j=0;j<limit;j++){
             a[i][j]=i+j;
         }    
     }
     
-    b=(int**)malloc(sizeof(int)*limit);
+    b=(int**)malloc(sizeof(int*)*limit);
     for(i=0;i<limit;i++){
-        b[i]=(int*)malloc(sizeof(int)*limit);
+        b[i]=(int*)malloc(sizeof(int*)*limit);
         for(j=0;j<limit;j++){
             b[i][j]=i+j;
         }    
     }
     
-    transposeB=(int**)malloc(sizeof(int)*limit);
+    transposeB=(int**)malloc(sizeof(int*)*limit);
     for(i=0;i<limit;i++){
-        transposeB[i]=(int*)malloc(sizeof(int)*limit);
+        transposeB[i]=(int*)malloc(sizeof(int*)*limit);
         for(j=0;j<limit;j++){
             transposeB[i][j]=b[j][i];
         }    
     }
     
-    clock_t t;
-    t = clock();
+     clock_t t;
+     t = clock();
     
-    mul=(int**)malloc(sizeof(int)*limit);
+    mul=(int**)malloc(sizeof(int*)*limit);
     for(i=0;i<limit;i++){
-        mul[i]=(int*)malloc(sizeof(int)*limit);
+        mul[i]=(int*)malloc(sizeof(int*)*limit);
         for(j=0;j<limit;j++){
             mul[i][j]=0;
             for(k=0;k<limit;k++){
@@ -53,7 +54,8 @@ int main(int argc, char*argv[])
     
     clock_t t2;
     t2 = clock();
-    
+          
+  
     for(i=0;i<limit;i++){
         for(j=0;j<limit;j++){
             mul[i][j]=0;
@@ -62,12 +64,12 @@ int main(int argc, char*argv[])
             }
         }    
     }
-    
-    t2 = clock() - t2;
+     t2 = clock() - t2;
     
    double time_taken = ((double)t)/CLOCKS_PER_SEC;
    double time_taken2 = ((double)t2)/CLOCKS_PER_SEC;
    printf("The program took %f seconds to execute(cache friendly)", time_taken);
+   printf("\n");
    printf("The program took %f seconds to execute(traditional method)", time_taken2);
-    printf("\n");
+   printf("\n");
 }
